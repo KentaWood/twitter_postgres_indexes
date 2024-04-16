36c7cd4d63b02fb86a8721717497538c47eab67a
Alexander Wood <115967095+KentaWood@users.noreply.github.com>
2024-04-16 10:06:41 -0700

    Update 05.sql

--- sql.normalized_batch/05.sql
+++ sql.normalized_batch/05.sql
@@ -10,6 +10,7 @@ FROM (
         tag
     FROM tweets
     JOIN tweet_tags USING (id_tweets)
+    --needs to use gin index
     WHERE to_tsvector('english',text)@@to_tsquery('english','coronavirus')
       AND lang='en'
 ) t
